d677780cde8675c614f4c802b3ff4f6b01c6ca2f
Memories6 <1371910434@qq.com>
2020-02-06 22:21:56 +0800

    寻找迷宫最短路径优化完善

--- maze/maze.go
+++ maze/maze.go
@@ -41,10 +41,19 @@ func (p Point) at(grid [][]int) (int, bool) {
 // end Point 地图中终点
 func walk(maze [][]int, start Point, end Point) [][]int {
 	// 二位数组切片初始化如下
-	// 初始化稍后要走的路
+	// 初始化一个和 maze 一样的切片
 	steps := make([][]int, len(maze))
 	for i := range steps {
 		steps[i] = make([]int, len(maze[i]))
+	}
+
+	// 显示的时候，会显示将源切片和走过的路径全显示出来
+	copy(steps, maze) // 目标切片， 源切片
+
+	// 出口是一堵墙
+	if steps[end.i][end.j] == 1 {
+		fmt.Println("这是一堵墙，不能作为终点")
+		return nil
 	}
 	
 	// 开始探险
@@ -53,52 +62,59 @@ func walk(maze [][]int, start Point, end Point) [][]int {
 		cur := q[0] // 获取起点的坐标
 		q = q[1:] // 删除当前的位置
 
-		fmt.Println("cur", cur)
-		fmt.Println("q", q)
-
 		for _, dir := range dirs {
-			next := cur.add(dir) // 寻找下一个坐标的正确位置
+			nextPoint := cur.add(dir) // 移动到下一个坐标
 			// 是否越界
-			val, ok := next.at(maze)
+			val, ok := nextPoint.at(maze)
 			if !ok || val == 1 {
+				// 数组越界 或者 当前位置是一堵墙
 				continue
 			}
 
 			// 是否走过
-			val, ok = next.at(steps)
+			val, ok = nextPoint.at(steps)
 			if !ok || val != 0 {
 				continue
 			}
 
 			// 回到起点
-			if next == start {
+			if nextPoint == start {
 				continue
 			}
 
-			curSteps, _ := cur.at(steps)
-			steps[next.i][next.j] = curSteps + 1
+			curSteps, _ := cur.at(steps) // 获取到当前点的值
+			steps[nextPoint.i][nextPoint.j] = curSteps + 1
 
-			q = append(q, next)
+			q = append(q, nextPoint)
+		}
+
+		// 当找到迷宫终点时，我们就没必要继续往下走了，跳出结束
+		if steps[end.i][end.j] != 0 {
+			break
 		}
 	}
 	return steps
 }
 
-func main() {
+func tempFile() {
 	var mazeMap [][]int = make([][]int, 6)
 	for i  := range mazeMap {
         mazeMap[i] = make([]int, 5)
-    }
+	}
+	// 值为 1， 表示当前位置是一堵墙
 	mazeMap[0][1] = 1
 	mazeMap[1][3] = 1
 	mazeMap[2][1] = 1
 	mazeMap[2][3] = 1
 	mazeMap[3][0] = 1
 	mazeMap[3][1] = 1
-	mazeMap[3][2] = 1
+	// mazeMap[3][2] = 1
+	// mazeMap[3][3] = 1
+	// mazeMap[3][4] = 1
 	mazeMap[4][1] = 1
 	mazeMap[4][4] = 1
 	mazeMap[5][1] = 1
+	// mazeMap[5][4] = 1
 	for _, v := range mazeMap {
 		for _, v1 := range v {
 			fmt.Print(v1, " ")
@@ -106,12 +122,21 @@ func main() {
 		fmt.Println()
 	}
 
-	data := walk(mazeMap, Point{0, 0}, Point{len(mazeMap) - 1, len(mazeMap) - 1})
+	data := walk(mazeMap, Point{0, 0}, Point{3, 2})
+	// data := walk(mazeMap, Point{0, 0}, Point{len(mazeMap) - 1, len(mazeMap[0]) - 1})
+	if data != nil && data[3][2] == 0 {
+	// if data != nil && data[len(data)-1][len(data[len(mazeMap)-1])-1] == 0 {
+		fmt.Println("不能抵达终点")
+		return 
+	}
 	for _, row := range data {
 		for _, val := range row {
 			fmt.Printf("%3d", val)
 		}
 		fmt.Println()
 	}
+}
 
+func main() {
+	tempFile()
 }
